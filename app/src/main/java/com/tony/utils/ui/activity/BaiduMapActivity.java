1db11e15e395a5f7795886c09778516dcbae8ebe
gitzsq <gitmima123>
2019-05-08 15:03:08 +0800

    添加自定义热力图

--- app/src/main/java/com/tony/utils/ui/activity/BaiduMapActivity.java
+++ app/src/main/java/com/tony/utils/ui/activity/BaiduMapActivity.java
@@ -1,8 +1,18 @@
 package com.tony.utils.ui.activity;
 
+import android.graphics.Color;
+
 import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.Gradient;
+import com.baidu.mapapi.map.HeatMap;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.model.LatLng;
 import com.tony.utils.R;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Random;
 
 import butterknife.BindView;
 
@@ -12,6 +22,10 @@ public class BaiduMapActivity extends BaseActivity {
     @BindView(R.id.bmapview)
     MapView mMapView;
     private BaiduMap mBaiduMap;
+    private Gradient gradient;
+    private HeatMap heatMap;
+
+    private List<LatLng> latLngList = new ArrayList<>();
 
     @Override
     protected int getLayoutId() {
@@ -24,10 +38,39 @@ public class BaiduMapActivity extends BaseActivity {
         //普通地图
         mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
         //卫星地图
-        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_SATELLITE);
+//        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_SATELLITE);
 
         //开启热力图
         mBaiduMap.setBaiduHeatMapEnabled(true);
+        //设置渐变颜色值
+        int[] DEFAULT_GRADIENT_COLORS = {Color.rgb(102, 225, 0), Color.rgb(255, 0, 0)};
+//设置渐变颜色起始值
+        float[] DEFAULT_GRADIENT_START_POINTS = {0.2f, 1f};
+//构造颜色渐变对象
+        Gradient gradient = new Gradient(DEFAULT_GRADIENT_COLORS, DEFAULT_GRADIENT_START_POINTS);
+        //以下数据为随机生成地理位置点，开发者根据自己的实际业务，传入自有位置数据即可
+        List<LatLng> randomList = new ArrayList<LatLng>();
+
+        Random r = new Random();
+        for (int i = 0; i < 500; i++) {
+            // 116.220000,39.780000 116.570000,40.150000
+            int rlat = r.nextInt(370000);
+            int rlng = r.nextInt(370000);
+            int lat = 39780000 + rlat;
+            int lng = 116220000 + rlng;
+            LatLng ll = new LatLng(lat / 1E6, lng / 1E6);
+            randomList.add(ll);
+        }
+
+        //构造HeatMap
+//在大量热力图数据情况下，build过程相对较慢，建议放在新建线程实现
+        HeatMap mCustomHeatMap = new HeatMap.Builder()
+                .data(randomList)
+                .gradient(gradient)
+                .build();
+
+//在地图上添加自定义热力图
+        mBaiduMap.addHeatMap(mCustomHeatMap);
     }
 
     @Override
--- app/src/main/java/com/tony/utils/utils/pic/pictools/Bimp.java
+++ app/src/main/java/com/tony/utils/utils/pic/pictools/Bimp.java
@@ -1,6 +1,7 @@
 package com.tony.utils.utils.pic.pictools;
 
 import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -9,6 +10,8 @@ import java.util.List;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.util.Log;
 
 public class Bimp
 {
@@ -20,30 +23,46 @@ public class Bimp
 
 
 
+//	public static Bitmap revitionImageSize(String path) throws IOException
+//	{
+//		BufferedInputStream in = new BufferedInputStream(new FileInputStream(
+//				new File(path)));
+//		BitmapFactory.Options options = new BitmapFactory.Options();
+//		options.inJustDecodeBounds = true;
+//		BitmapFactory.decodeStream(in, null, options);
+//		in.close();
+//		int i = 0;
+//		Bitmap bitmap = null;
+//		while (true)
+//		{
+//			if ((options.outWidth >> i <= 1000)
+//					&& (options.outHeight >> i <= 1000))
+//			{
+//				in = new BufferedInputStream(
+//						new FileInputStream(new File(path)));
+//				options.inSampleSize = (int) Math.pow(2.0D, i);
+//				options.inJustDecodeBounds = false;
+//				bitmap = BitmapFactory.decodeStream(in, null, options);
+//				break;
+//			}
+//			i += 1;
+//		}
+//		return bitmap;
+//	}
+    //图片占用内存压缩
 	public static Bitmap revitionImageSize(String path) throws IOException
 	{
-		BufferedInputStream in = new BufferedInputStream(new FileInputStream(
-				new File(path)));
-		BitmapFactory.Options options = new BitmapFactory.Options();
-		options.inJustDecodeBounds = true;
-		BitmapFactory.decodeStream(in, null, options);
-		in.close();
-		int i = 0;
-		Bitmap bitmap = null;
-		while (true)
-		{
-			if ((options.outWidth >> i <= 1000)
-					&& (options.outHeight >> i <= 1000))
-			{
-				in = new BufferedInputStream(
-						new FileInputStream(new File(path)));
-				options.inSampleSize = (int) Math.pow(2.0D, i);
-				options.inJustDecodeBounds = false;
-				bitmap = BitmapFactory.decodeStream(in, null, options);
-				break;
-			}
-			i += 1;
-		}
-		return bitmap;
+		Bitmap	bit = BitmapFactory.decodeFile(path);
+
+		Log.i("wechat", "压缩前图片的大小" + (bit.getByteCount() / 1024 / 1024)
+				+ "M宽度为" + bit.getWidth() + "高度为" + bit.getHeight());
+		Matrix matrix = new Matrix();
+		matrix.setScale(0.5f, 0.5f);
+		Bitmap bm = Bitmap.createBitmap(bit, 0, 0, bit.getWidth(),
+				bit.getHeight(), matrix, true);
+		Log.i("wechat", "压缩后图片的大小" + (bm.getByteCount() / 1024 / 1024)
+				+ "M宽度为" + bm.getWidth() + "高度为" + bm.getHeight());
+		return bm;
 	}
+
 }
